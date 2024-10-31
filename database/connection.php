290dc2adc25799a47279e513d47dc5a145b890a5
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-10-31 15:38:45 +0000

    change passwd BD

--- database/connection.php
+++ database/connection.php
@@ -1,5 +1,5 @@
 <?php
-@$link = new mysqli('localhost', 'shopuser', '123456', 'shop');
+@$link = new mysqli('localhost', 'shopuser', '1234', 'shop');
 $error = $link->connect_errno;
 if ($error != null) {
   echo "<p>Error $error conectando a la base de datos: $link->connect_error</p>";
